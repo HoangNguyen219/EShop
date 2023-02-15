63b09244fb0186b43fb57a258f98bdcc016ea65e
Hoang Nguyen <>
2023-02-15 10:07:39 +0700

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const expressHandlebars = require('express-handlebars');
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'hbs');
+
+app.use('/', require('./routes/indexRouter'));
+
+app.use((req, res, next) => {
+    res.status(404).render('error', {message: 'File not Found'});
+})
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    res.status(500).render('error', {message: 'Internal Server Error'});
+}) 
+
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+});
+
+
