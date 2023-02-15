bfce96a6ab2431b6cf664143521de1a16550a5f1
Hoang Nguyen <>
2023-02-15 20:37:43 +0700

    filter products with brands

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,8 +2,18 @@ let controller = {};
 const models = require('../models');
 
 controller.show = async (req, res) => {
+    let brand = req.query.brand;
+
+    let brands = await models.Brand.findAll({
+        include: [{
+            model: models.Product
+        }]
+    })
+    res.locals.brands = brands;
+    console.log(brands);
+
+
     let category = req.query.category;
-    console.log(category);
     let categories = await models.Category.findAll({
         include: [{
             model: models.Product
@@ -17,6 +27,9 @@ controller.show = async (req, res) => {
     }
     if(category) {
         options.where.categoryId = category
+    }
+    if(brand) {
+        options.where.brandId = brand
     }
     let products = await models.Product.findAll(options);
     res.locals.products = products;
