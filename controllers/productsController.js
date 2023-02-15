0ef1847245be9470d454cd3d2222dff32d8e88d9
Hoang Nguyen <>
2023-02-15 20:25:20 +0700

    filter products with category

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,9 +2,23 @@ let controller = {};
 const models = require('../models');
 
 controller.show = async (req, res) => {
-    let products = await models.Product.findAll({
+    let category = req.query.category;
+    console.log(category);
+    let categories = await models.Category.findAll({
+        include: [{
+            model: models.Product
+        }]
+    })
+    res.locals.categories = categories;
+
+    let options = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice',],
-    });
+        where: {}
+    }
+    if(category) {
+        options.where.categoryId = category
+    }
+    let products = await models.Product.findAll(options);
     res.locals.products = products;
     res.render('product-list');
 }
