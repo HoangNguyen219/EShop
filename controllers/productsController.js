4970429ce0e2eed62c5d403e96445b6819325056
Hoang Nguyen <>
2023-02-24 17:07:41 +0700

    filter product with tag
    n-n relationship

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,7 +2,9 @@ let controller = {};
 const models = require('../models');
 
 controller.show = async (req, res) => {
-    let brand = req.query.brand;
+    let brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
+    let category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
+    let tag = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
 
     let brands = await models.Brand.findAll({
         include: [{
@@ -10,10 +12,7 @@ controller.show = async (req, res) => {
         }]
     })
     res.locals.brands = brands;
-    console.log(brands);
 
-
-    let category = req.query.category;
     let categories = await models.Category.findAll({
         include: [{
             model: models.Product
@@ -21,16 +20,28 @@ controller.show = async (req, res) => {
     })
     res.locals.categories = categories;
 
+    let tags = await models.Tag.findAll();
+    res.locals.tags = tags;
+
+
     let options = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice',],
         where: {}
     }
-    if(category) {
+    if (category > 0) {
         options.where.categoryId = category
     }
-    if(brand) {
+    if (brand > 0) {
         options.where.brandId = brand
     }
+    if (tag > 0) {
+        options.include = [{
+            model: models.Tag,
+            where: { id: tag }
+        }]
+    }
+
+
     let products = await models.Product.findAll(options);
     res.locals.products = products;
     res.render('product-list');
